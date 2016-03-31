d585963d25454b2dc274208ffe36002e52943b2a
unknown <>
2016-03-31 15:46:53 +0900

    Added variables and methods -"PlanType"

--- src/main/java/com/team03/week04/PlanType.java
+++ src/main/java/com/team03/week04/PlanType.java
@@ -1,5 +1,27 @@
 package com.team03.week04;
 
 public class PlanType {
-
+	private double additionalLineRate;
+	private double ratePerExcessMinute;
+	private int includedMinutes;
+	private double basicMonthlyRate;
+	
+	public double getAdditionalLineRate() {
+		return additionalLineRate;
+	}
+	public double getRatePerExcessMinute() {
+		return ratePerExcessMinute;
+	}
+	public int getIncludedMinutes() {
+		return includedMinutes;
+	}
+	public double getBasicMonthlyRate() {
+		return basicMonthlyRate;
+	}
+	public double getFamilyDistcount() {
+		return familyDistcount;
+	}
+	private double familyDistcount;
 }
+
+
