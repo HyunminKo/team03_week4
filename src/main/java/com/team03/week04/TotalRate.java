45e3b45c827bf68f9917249cf3bfd83a808d8499
seojungwon <garden715@gmail.com>
2016-03-31 16:40:44 +0900

    edit variable, add methods 1. public totalRate to private 2. calTotalRate(), getTotalRate() method

--- src/main/java/com/team03/week04/TotalRate.java
+++ src/main/java/com/team03/week04/TotalRate.java
@@ -1,13 +1,26 @@
 package com.team03.week04;
 
 public class TotalRate {
-	public double totalRate = 0;
+	private double totalRate = 0;
+	private double lineCost = 0;
+	private double usedCost = 0;
 	
 	public int calNumberOfLine(User user, PlanType planType) {
+		// 
 		return 0;
 	}
 	
 	public double calMinuteUsed(User user, PlanType planType) {
+		//
 		return 0;
+	}
+	
+	public double calTotalRate() {
+		totalRate = lineCost + usedCost;
+		return totalRate;
+	}
+	
+	public double getTotalRate() {
+		return this.totalRate;
 	}
 }
