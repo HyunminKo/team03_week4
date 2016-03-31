23e988f43c225845774dbc3b38f77d5947a904ba
Jeongsoo Ha <jeongsoo931210@gmail.com>
2016-03-31 16:38:29 +0900

    Change content to silver class

--- src/main/java/com/team03/week04/Silver.java
+++ src/main/java/com/team03/week04/Silver.java
@@ -1,5 +1,11 @@
 package com.team03.week04;
 
 public class Silver extends PlanType{
-
+	Silver(){
+		this.setBasicMonthlyRate(29.95);
+		this.setAdditionalLineRate(21.50);
+		this.setFamilyDistcount(5.0);
+		this.setIncludedMinutes(500);
+		this.setRatePerExcessMinute(0.54);
+	}
 }
