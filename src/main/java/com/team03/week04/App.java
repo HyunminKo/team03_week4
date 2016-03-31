106892bda65e77643b1969297ed1010de6f8c111
Kohyunmin <hyunmin4578@gmail.com>
2016-03-31 16:38:02 +0900

    change content to Gold class

--- src/main/java/com/team03/week04/App.java
+++ src/main/java/com/team03/week04/App.java
@@ -8,6 +8,5 @@ public class App {
 	private App(){
 	}
     public static void main( String[] args ) {
-    	
     }
 }
--- src/main/java/com/team03/week04/Gold.java
+++ src/main/java/com/team03/week04/Gold.java
@@ -1,5 +1,11 @@
 package com.team03.week04;
 
 public class Gold extends PlanType{
-
+	Gold(){
+		this.setBasicMonthlyRate(49.95);
+		this.setAdditionalLineRate(14.50);
+		this.setFamilyDistcount(5.0);
+		this.setIncludedMinutes(1000);
+		this.setRatePerExcessMinute(0.45);
+	}
 }
