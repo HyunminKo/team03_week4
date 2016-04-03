f33b56940601001d4e7072a2dba2ec4a603f0e99
seojungwon <garden715@gmail.com>
2016-04-03 16:06:33 +0900

    edit Gold, Silver class - static variable

--- src/main/java/com/team03/week04/Silver.java
+++ src/main/java/com/team03/week04/Silver.java
@@ -1,17 +1,17 @@
 package com.team03.week04;
 
 public class Silver extends PlanType{
-	static final double silverBasicMonthlyRate = 29.95;
-	static final double silverAdditionalLineRate = 21.50;
-	static final double silverFamilyDistcount = 5.0;
-	static final int silverIncludeMinutes = 500;
-	static final double silverRatePerExcessMinute = 0.54;
+	static final double SILVER_BASIC_MONTHLY_RATE = 29.95;
+	static final double SILVER_BASIC_ADDITIONAL_LINE_RATE = 21.50;
+	static final double SILVER_FAMILY_DISTCOUNT = 5.0;
+	static final int SILVER_INCLUDE_MINUTES = 500;
+	static final double SILVER_RATE_PER_EXCESS_MINUTE = 0.54;
 	
 	public Silver(){
-		this.setBasicMonthlyRate(silverBasicMonthlyRate);
-		this.setAdditionalLineRate(silverAdditionalLineRate);
-		this.setFamilyDistcount(silverFamilyDistcount);
-		this.setIncludedMinutes(silverIncludeMinutes);
-		this.setRatePerExcessMinute(silverRatePerExcessMinute);
+		this.setBasicMonthlyRate(SILVER_BASIC_MONTHLY_RATE);
+		this.setAdditionalLineRate(SILVER_BASIC_ADDITIONAL_LINE_RATE);
+		this.setFamilyDistcount(SILVER_FAMILY_DISTCOUNT);
+		this.setIncludedMinutes(SILVER_INCLUDE_MINUTES);
+		this.setRatePerExcessMinute(SILVER_RATE_PER_EXCESS_MINUTE);
 	}
 }
