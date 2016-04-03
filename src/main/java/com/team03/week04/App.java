792f2c7a842ebdbac06ffc3a41d11f780b11784c
Jeongsoo Ha <jeongsoo931210@gmail.com>
2016-04-03 16:34:34 +0900

    commit for pull

--- src/main/java/com/team03/week04/App.java
+++ src/main/java/com/team03/week04/App.java
@@ -1,11 +1,16 @@
 package com.team03.week04;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
+
 
 public class App {
+	public static Logger LOG = Logger.getLogger("InfoLogging");
+
 	private App(){
 	}
     public static void main( String[] args ) {
+    	
     	Scanner scan = new Scanner(System.in);
     	User user = new User();
     	PlanType planType = null;
@@ -13,7 +18,7 @@ public class App {
     	boolean go = true;
     	
     	while(go) {
-    		System.out.println("User의 요금제를 입력하세요. ");
+    		LOG.info("User의 요금제를 입력하세요.");
     		user.setPlanTypeName(scan.next());
 
     		if(user.getPlanTypeName().equals("Gold")) {
@@ -23,37 +28,37 @@ public class App {
     			planType = new Silver();
     			go = false;
     		}else{
-    			System.out.println("잘못된 입력값입니다.");
+    			LOG.info("잘못된 입력값입니다.");
     		}
     		
     	}
     	
     	go = true;
     	while(go){
-    	System.out.println("User의 회선 수를 입력하세요. ");
+    	LOG.info("User의 회선 수를 입력하세요.");
     	user.setNumberOfLines(scan.nextInt());
     	if(user.getNumberOfLines()<=0)
-    		System.out.println("잘못된 입력값입니다.");
+    		LOG.info("잘못된 입력값입니다.");
     	else
     		go = false;
     	}
     	
     	go = true;
     	while(go){
-    	System.out.println("User의 사용시간을 입력하세요. ");
+    	LOG.info("User의 사용시간을 입력하세요.");
     	user.setMinutesUsed(scan.nextInt());
     	if(user.getMinutesUsed()<=0)
-    		System.out.println("잘못된 입력값입니다.");
+    		LOG.info("잘못된 입력값입니다.");
     	else
     		go = false;
     	}
     	
 
-    	System.out.println("User의 요금제 : " + user.getPlanTypeName());
+    	LOG.info("User의 요금제 : " + user.getPlanTypeName());
 
-    	System.out.println("User의 회선 수 : " + user.getNumberOfLines());
+    	LOG.info("User의 회선 수 : " + user.getNumberOfLines());
 
-    	System.out.println("User의 사용시간 : "+ user.getMinutesUsed());
+    	LOG.info("User의 사용시간 : "+ user.getMinutesUsed());
     	
     	totalRate.calTotalRate(user, planType);    	
     }
--- src/main/java/com/team03/week04/TotalRate.java
+++ src/main/java/com/team03/week04/TotalRate.java
@@ -4,21 +4,19 @@ public class TotalRate {
 	private double totalRate = 0;
 	private double lineCost = 0;
 	private double usedCost = 0;
-
+	String resultofLines="",resultofUsed="";
 	// 기본요금+회선에 대한 추가요금(요금제에 따라서 기본요금과 추가되는 요금이 다르다)
 	public double calNumberOfLine(User user, PlanType planType) {
-
 		int numberOfLines = user.getNumberOfLines();
 		lineCost = planType.getBasicMonthlyRate();
 		if (numberOfLines > 1 && numberOfLines <= 3) {
 			lineCost += ((numberOfLines - 1) * planType.getAdditionalLineRate());
-			System.out.print(planType.getBasicMonthlyRate()+" + ( "+numberOfLines+" * "+planType.getAdditionalLineRate()+" )");
+			resultofLines+=(planType.getBasicMonthlyRate()+" + ( "+numberOfLines+" * "+planType.getAdditionalLineRate()+" )");
 		} else if (numberOfLines >= 4) {
 			lineCost += (((numberOfLines - 3) * planType.getFamilyDistcount())+planType.getAdditionalLineRate()*2);
-			System.out.print(planType.getBasicMonthlyRate()+" + ( "+2+" * "+planType.getAdditionalLineRate()+" ) + "+
-			" ( "+(numberOfLines-2)+" * "+planType.getFamilyDistcount()+" )");
+			resultofLines+=(planType.getBasicMonthlyRate()+" + ( "+2+" * "+planType.getAdditionalLineRate()+" ) + "+
+			" ( "+(numberOfLines-3)+" * "+planType.getFamilyDistcount()+" )");
 		}
-		
 		return lineCost;
 	}
 
@@ -31,21 +29,23 @@ public class TotalRate {
 		
 		if (minutesUsed > includedMinutes) {
 			usedCost = (minutesUsed - includedMinutes) * ratePerExcessMinute;
-			System.out.print(" + ( "+(minutesUsed - includedMinutes)+" * "+ratePerExcessMinute+" ) ");
+			resultofUsed+=(" + ( "+(minutesUsed - includedMinutes)+" * "+ratePerExcessMinute+" ) = ");
 		} else {
+			resultofUsed+=" = ";
 			usedCost = 0;
 		}
-			
 		return usedCost;
 	}
 
 	public double calTotalRate(User user, PlanType planType) {
 		totalRate = calNumberOfLine(user, planType) + calMinuteUsed(user, planType);
-		System.out.printf(" = %.2f",totalRate);
+		totalRate = Double.parseDouble(String.format("%.2f", totalRate));
+		App.LOG.info(resultofLines+resultofUsed+totalRate+"$");
 		return totalRate;
 	}
 
 	public double getTotalRate() {
+		
 		return this.totalRate;
 	}
 }
