d93cca1fbdf48660203f6a840b63ec711d8574b1
seojungwon <garden715@gmail.com>
2016-04-03 15:50:43 +0900

    edit Gold.java, Silver.java class - add the constant variable

--- src/main/java/com/team03/week04/Gold.java
+++ src/main/java/com/team03/week04/Gold.java
@@ -1,11 +1,17 @@
 package com.team03.week04;
 
 public class Gold extends PlanType{
+	final double goldBasicMonthlyRate = 49.95;
+	final double goldAdditionalLineRate = 14.50;
+	final double goldFamilyDistcount = 5.0;
+	final int goldInculdedMinutes = 1000;
+	final double goldRatePerExcessMinute = 0.45;
+	
 	public Gold(){
-		this.setBasicMonthlyRate(49.95);
-		this.setAdditionalLineRate(14.50);
-		this.setFamilyDistcount(5.0);
-		this.setIncludedMinutes(1000);
-		this.setRatePerExcessMinute(0.45);
+		this.setBasicMonthlyRate(goldBasicMonthlyRate);
+		this.setAdditionalLineRate(goldAdditionalLineRate);
+		this.setFamilyDistcount(goldFamilyDistcount);
+		this.setIncludedMinutes(goldInculdedMinutes);
+		this.setRatePerExcessMinute(goldRatePerExcessMinute);
 	}
 }
--- src/main/java/com/team03/week04/Silver.java
+++ src/main/java/com/team03/week04/Silver.java
@@ -1,11 +1,17 @@
 package com.team03.week04;
 
 public class Silver extends PlanType{
+	final double silverBasicMonthlyRate = 29.95;
+	final double silverAdditionalLineRate = 21.50;
+	final double silverFamilyDistcount = 5.0;
+	final int silverIncludeMinutes = 500;
+	final double silverRatePerExcessMinute = 0.54;
+	
 	public Silver(){
-		this.setBasicMonthlyRate(29.95);
-		this.setAdditionalLineRate(21.50);
-		this.setFamilyDistcount(5.0);
-		this.setIncludedMinutes(500);
-		this.setRatePerExcessMinute(0.54);
+		this.setBasicMonthlyRate(silverBasicMonthlyRate);
+		this.setAdditionalLineRate(silverAdditionalLineRate);
+		this.setFamilyDistcount(silverFamilyDistcount);
+		this.setIncludedMinutes(silverIncludeMinutes);
+		this.setRatePerExcessMinute(silverRatePerExcessMinute);
 	}
 }
