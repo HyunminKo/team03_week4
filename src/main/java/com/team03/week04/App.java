611641bf9223dc79e2895b74d9aa7d365621fe50
Larva <ssh9112@naver.com>
2016-04-03 15:41:20 +0900

    Edit App.java

--- src/main/java/com/team03/week04/App.java
+++ src/main/java/com/team03/week04/App.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class App {
 	private App(){
 	}
+	private static final String printedErrorMessage = "잘못된 입력값입니다.";
+	
     public static void main( String[] args ) {
     	Scanner scan = new Scanner(System.in);
     	User user = new User();
@@ -23,7 +25,7 @@ public class App {
     			planType = new Silver();
     			go = false;
     		}else{
-    			System.out.println("잘못된 입력값입니다.");
+    			System.out.println(printedErrorMessage);
     		}
     		
     	}
@@ -33,7 +35,7 @@ public class App {
 	    	System.out.println("User의 회선 수를 입력하세요. ");
 	    	user.setNumberOfLines(scan.nextInt());
 	    	if(user.getNumberOfLines()<=0)
-	    		System.out.println("잘못된 입력값입니다.");
+	    		System.out.println(printedErrorMessage);
 	    	else
 	    		go = false;
     	}
@@ -43,7 +45,7 @@ public class App {
 	    	System.out.println("User의 사용시간을 입력하세요. ");
 	    	user.setMinutesUsed(scan.nextInt());
 	    	if(user.getMinutesUsed()<=0)
-	    		System.out.println("잘못된 입력값입니다.");
+	    		System.out.println(printedErrorMessage);
 	    	else
 	    		go = false;
     	}
